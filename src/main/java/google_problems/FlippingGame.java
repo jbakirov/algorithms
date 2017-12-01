203e16ae8527f019b3f45746cb03afb9a90c435e
Jaloliddin Bakirov <bakirov.jaloliddin@gmail.com>
2017-11-30 22:29:56 -0600

    new problem

---
+++ src/main/java/google_problems/FlippingGame.java
@@ -0,0 +1,28 @@
+package google_problems;
+
+import java.util.Scanner;
+
+/**
+ * Created by jaloliddinbakirov on 11/27/17.
+ */
+public class FlippingGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[][] a = new int[n+1][4];
+
+        for (int i = 1; i <= a[0].length; i++){
+            a[0][i] = scanner.nextInt();
+        }
+
+        int sum1, sum2, sum3, sum4 = 0;
+        for (int i = 1; i <= a[0].length; i++){
+            a[0][i] = Math.max(a[0][i], a[0][i] + a[0][i-1]);
+            a[1][i] = Math.max(a[0][i-1], a[1][i-1]);
+//            a[2][i] = Math.max(a[])
+//            a[3][i] = Math.max(a[])
+        }
+    }
+}
